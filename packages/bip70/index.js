538a81af570b1dfe8e6b1b515ea00bb08700d772
Johann Barbie <johannbarbie@me.com>
2020-09-25 23:53:03 +0200

    latest payment server

--- packages/bip70/index.js
+++ packages/bip70/index.js
@@ -2,17 +2,40 @@ const express = require('express');
 const fs = require('fs');
 const bitcore = require('bitcore-lib');
 const PaymentProtocol = require('bitcore-payment-protocol');
-
+const bodyParser = require('body-parser');
 const file_with_x509_der_cert = fs.readFileSync('/home/ubuntu/bip70/cert.der');
 const file_with_x509_der_chain = fs.readFileSync('/home/ubuntu/bip70/chain.der');
 const file_with_x509_private_key = fs.readFileSync('/home/ubuntu/bip70/privkey.pem', 'utf8');
+const request = require("request");
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {res.send('hello world');});
+const headers = {
+  "content-type": "text/plain;"
+};
+const appJson = {
+  "Content-Type: application/json"
+};
+const USER = process.env.RPC_USER;
+const PASS = process.env.RPC_PASSWORD;
 
-app.get('/bla/', (req, res) => {res.send('hello world 2');});
+app.use(bodyParser.raw({ type:'*/*' }));
+
+function httpRequest(params) {
+  return new Promise(function(resolve, reject) {
+    const req = request(params, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+        console.log('data:', data);
+        resolve(body);
+      } else {
+        console.log('error:', response.body);
+        reject(response.body);
+      }
+    });
+  });
+}
 
 app.get('/:destination/:amount', (req, res) => {
 
@@ -36,8 +59,8 @@ app.get('/:destination/:amount', (req, res) => {
   details.set('outputs', output.message);
   details.set('time', now);
   details.set('expires', now + 60 * 60 * 24);
-  details.set('memo', 'A payment request from the merchant.');
-  details.set('payment_url', 'https://bip70.strudel.finance/payment');
+  details.set('memo', 'A request to enter the strudel.');
+  details.set('payment_url', 'https://bip70.strudel.finance/ack');
   details.set('merchant_data', Buffer.from(JSON.stringify({"foo": "bar"})));
   const certificates = new PaymentProtocol().makeX509Certificates();
   certificates.set('certificate', [file_with_x509_der_cert, file_with_x509_der_chain]);
@@ -60,6 +83,48 @@ app.get('/:destination/:amount', (req, res) => {
   });
   res.send(rawbody);
 });
+
+app.post('/ack', async (req, res) => { 
+  // Decode payment
+  const body = PaymentProtocol.Payment.decode(req.body);
+  const payment = new PaymentProtocol().makePayment(body);
+  const merchantData = payment.get('merchant_data');
+  const transactions = payment.get('transactions');
+  const txHash = transactions[0].toString('hex');
+  console.log('tx:', txHash);
+  const dataString = '{"jsonrpc":"1.0","id":"curltext","method":"sendrawtransaction","params":["' + txHash + '"]}';
+  const rpcOptions = {
+    url: `http://${USER}:${PASS}@bcd.strudel.finance:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+  const txHash = await httpRequest(rpcOptions);
+  console.log('txHash:', txHash);
+
+  const backendOptions = {
+    url: `https://j3x0y5yg6c.execute-api.eu-west-1.amazonaws.com/production/payment/${txHash}`,
+    method: "POST",
+    headers: appJson,
+    body: `{"txData":"${txHash}"}`
+  };
+  await httpRequest(backendOptions);
+    
+    // Make a payment acknowledgement
+  var ack = new PaymentProtocol().makePaymentACK();
+  ack.set('payment', payment.message);
+  ack.set('memo', 'Thank you for your payment!');
+  var rawBody = ack.serialize();
+    
+  res.set({
+    'Content-Type': PaymentProtocol.LEGACY_PAYMENT.BTC.PAYMENT_ACK_CONTENT_TYPE,
+    'Content-Length': rawBody.length,
+    'Content-Transfer-Encoding': 'binary'
+  });
+    
+  res.send(rawBody);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
