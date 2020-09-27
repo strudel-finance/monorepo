3ec48296e3ba361c9f2f919eae28a1e837ab3b35
Evgeni <evgeni@leapdao.org>
2020-09-27 13:26:16 +0200

    getBtcAddress function done

---
+++ packages/frontend/src/utils/encodeAddress.js
@@ -0,0 +1,35 @@
+//import { encode } from "./bech32";
+const encode = require("./segwit_addr").encode;
+
+module.exports = {
+    getBtcAddress: getBtcAddress,
+    //getBtcUri: getBtcUri,
+}
+
+const PREFIX = "ffffffffffff"; // prefix and suffix to be added when encoding to p2wsh address 
+const POSTFIX = "ffffffffffff";// not used otherwise
+const HRP = 'bc';
+
+const addrToProgram = (addr) => {
+    const re = /[0-9A-Fa-f]{2}/gi;
+    const tokens = (addr.match(re)); //split string to array
+    const program = tokens.map(t => parseInt(t, 16)); //convert hex numbers in string array to dec
+    return program;    
+}
+
+function getBtcAddress (ethAddress, isP2WSH=true) {
+    if (ethAddress.charAt(1) == 'x') ethAddress = ethAddress.substring(2);
+    
+    if (ethAddress.length != 40) return "Error: Wrong Ethereum address format";
+    
+    if (isP2WSH) ethAddress = PREFIX + ethAddress + POSTFIX;
+    
+    const program = addrToProgram(ethAddress);
+    let progLength = 20;
+    if (isP2WSH) progLength = 32;
+    if ((program.length != progLength)) return "Error: Wrong Ethereum address format";
+    
+    const btcAddress = encode(HRP, '0', program);
+    return btcAddress;
+}
+
