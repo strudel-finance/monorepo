44bd66b8f83dbec9fef02bd022510ff75b965f52
Evgeni <evgeni@leapdao.org>
2020-09-27 14:14:20 +0200

    js -> ts

--- packages/frontend/src/utils/encodeAddress.ts
+++ packages/frontend/src/utils/encodeAddress.ts
@@ -1,24 +1,24 @@
-//import { encode } from "./bech32";
-const encode = require("./segwit_addr").encode;
+import { encode } from "./segwit_addr";
+//const encode = require("./segwit_addr").encode;
 
-module.exports = {
+/*module.exports = {
     getBtcAddress: getBtcAddress,
     getBtcUri: getBtcUri,
-}
+}*/
 
 const PREFIX = "ffffffffffff"; // prefix and suffix to be added when encoding to p2wsh address 
 const POSTFIX = "ffffffffffff";// not used otherwise
 const HRP = 'bc';
 const URI_PREFIX = 'bitcoin:';
 
-const addrToProgram = (addr) => {
+const addrToProgram = (addr: string) => {
     const re = /[0-9A-Fa-f]{2}/gi;
     const tokens = (addr.match(re)); //split string to array
     const program = tokens.map(t => parseInt(t, 16)); //convert hex numbers in string array to dec
     return program;    
 }
 
-function getBtcAddress (ethAddress, isP2WSH=true) {
+export function getBtcAddress (ethAddress: string, isP2WSH=true) {
     if (ethAddress.charAt(1) == 'x') ethAddress = ethAddress.substring(2);
     
     if (ethAddress.length != 40) return "Error: Wrong Ethereum address format";
@@ -34,8 +34,8 @@ function getBtcAddress (ethAddress, isP2WSH=true) {
     return btcAddress;
 }
 
-function getBtcUri (ethAddress, amount, url, isP2WSH=true) {
+export function getBtcUri (ethAddress: string, amount: string, isP2WSH=true) {
     const btcAddress = getBtcAddress(ethAddress, isP2WSH);
     if (btcAddress == "Error: Wrong Ethereum address format") return btcAddress;
-    return URI_PREFIX.concat(btcAddress,"?amount=",amount.toString(),"&r=",url);
+    return URI_PREFIX.concat(btcAddress,"?amount=",amount);
 }
