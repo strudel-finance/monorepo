a0664966d6d25f15546efc1768806c534ec14b27
Keno Budde <Clearwood@users.noreply.github.com>
2020-10-07 13:45:48 +0200

    restructuring component

--- packages/frontend/src/views/Home/Home.tsx
+++ packages/frontend/src/views/Home/Home.tsx
@@ -17,20 +17,15 @@ import Balances from './components/Balances'
 import BalanceStrudel from './components/BalanceStrudel'
 import Lottie, { MobileLottie } from '../../components/Lottie'
 import formatAddress from '../../utils/formatAddress'
-import showError, { handleErrors } from '../../utils/showError'
 import useModal from '../../hooks/useModal'
 import BurnModal from './components/BurnModal'
-import useInterval from '../../hooks/useInterval'
 import Container from '@material-ui/core/Container'
 import TransactionsTableContainer from '../../components/TransactionsTableContainer'
 import { StyledLink } from '../../components/Footer/components/Nav'
+import { Transaction, LoadingStatus } from '../../types/types'
 
 import { makeStyles, withStyles } from '@material-ui/core'
-import { Transaction, LoadingStatus } from '../../types/types'
-import sb from 'satoshi-bitcoin'
-import { apiServer } from '../../constants/backendAddresses'
 import { startDate } from '../../constants/countdown'
-import RollbarErrorTracking from '../../errorTracking/rollbar'
 import AstroFlying from '../../assets/img/AstroFlying.png'
 
 import useVBTC from '../../hooks/useVBTC'
@@ -68,41 +63,15 @@ const MyStyledLink = styled(StyledLink)`
   font-size: 50px;
 `
 const Home: React.FC = () => {
-  const POLL_DURATION_TXS = 1500
-  const BTC_ACCEPTANCE = 6
   const [val, setVal] = useState('0')
   const [lastRequest, setLastRequest] = useState(undefined)
-  const [transactions, setTransactions] = useState([])
-  const [confirmations, setConfirmations] = useState({})
-  const [address, setAddress] = useState(
-    '0x0000000000000000000000000000000000000000',
-  )
-  const [isLoading, setLoading] = useState({})
+
   const [isCountComplete, setCountComplete] = useState(false)
 
   const wallet = useWallet()
   const account = wallet.account
   const [onPresentWalletProviderModal] = useModal(<WalletProviderModal />)
 
-  interface SoChainConfirmed {
-    status: string
-    data: {
-      confirmations: number
-    }
-  }
-  interface AccountRequest {
-    account: string
-    burns: [
-      {
-        amount: string // satoshis
-        dateCreated: Date
-        btcTxHash: string
-        status: string
-        burnOutputIndex: string
-        ethTxHash?: string
-      },
-    ]
-  }
   const usePrevious = (value: any) => {
     const ref = useRef()
     useEffect(() => {
@@ -111,156 +80,22 @@ const Home: React.FC = () => {
     return ref.current
   }
 
-  const isAccountRequest = (
-    res: AccountRequest | void,
-  ): res is AccountRequest => {
-    if ((res as AccountRequest).burns) {
-      return true
-    }
-    return false
-  }
   const checkAndRemoveLastRequest = () => {
     if (lastRequest !== undefined) {
       window.localStorage.removeItem(account)
       setLastRequest(undefined)
     }
   }
-  const handleLoading = (ls: LoadingStatus) => {
-    let tempLoading = isLoading
-    tempLoading[ls.tx] = ls.status
-    setLoading(tempLoading)
-  }
 
-  const handleTransactionUpdate = async () => {
-    if (wallet.status === 'connected') {
-      let res = await fetch(`${apiServer}/production/account/${account}`)
-        .then(handleErrors)
-        .then((response) => response.json())
-        .then((res: AccountRequest) => res)
-        .catch((e) => {
-          //forget error
-          if (e.message != 404) {
-            showError('Problem fetching account: ' + e.message)
-            RollbarErrorTracking.logErrorInRollbar(
-              'Problem fetching account: ' + e.message,
-            )
-          }
-          return undefined
-        })
-      if (res === undefined) {
-        return
-      }
-      let resNew: Transaction[] = []
-      if (isAccountRequest(res)) {
-        res.burns.map((tx, i) => {
-          let txNew: Transaction = {
-            ethAddress: account,
-            value: sb.toBitcoin(tx.amount),
-            txCreatedAt: new Date(tx.dateCreated),
-            btcTxHash: tx.btcTxHash,
-            burnOutputIndex: tx.burnOutputIndex,
-            confirmed: tx.status === 'paid' ? true : false,
-          }
-          if (tx.ethTxHash) {
-            txNew.ethTxHash = tx.ethTxHash
-          }
-          resNew.push(txNew)
-        })
-        resNew = resNew.sort((txa, txb) => {
-          return (txa.txCreatedAt ?? 0) < (txb?.txCreatedAt ?? 0) ? 1 : -1
-        })
-        if (transactions.length === 0) {
-          if (
-            lastRequest !== undefined &&
-            resNew[0].txCreatedAt > lastRequest.txCreatedAt
-          ) {
-            checkAndRemoveLastRequest()
-          }
-          setTransactions(resNew)
-        } else if (resNew.length > transactions.length) {
-          setTransactions(resNew)
-          checkAndRemoveLastRequest()
-        }
-      }
-    }
-  }
   const previousAccount = usePrevious(account)
-  /*
-  const vbtc = useVBTC()
-  useEffect(() => {
-    showError('hey')
-    console.log(vbtc)
-  }, [vbtc])
-  */
-  useEffect(() => {
-    if (account == null || previousAccount !== account) {
-      setTransactions([])
-      setLastRequest(undefined)
-    }
-    if (previousAccount !== account) {
-      if (lastRequest === undefined && localStorage.hasOwnProperty(account)) {
-        let tx = JSON.parse(window.localStorage.getItem(account))
-        tx.txCreatedAt = new Date(tx.txCreatedAt)
-        setLastRequest(tx)
-      }
-      // get transactions at first
-      handleTransactionUpdate()
-    }
-    return undefined
-  }, [account])
-
-  useInterval(async () => {
-    if (account != null) {
-      await handleTransactionUpdate()
-      if (transactions.length > 0) {
-        let transactionsT: Transaction[] = transactions
-        let transactionsWithLowConfirmations = transactionsT.filter(
-          (tx) =>
-            !tx.confirmed &&
-            (confirmations[tx.btcTxHash] < BTC_ACCEPTANCE ||
-              confirmations[tx.btcTxHash] === undefined),
-        )
-
-        let highConfirmations = {}
-        Object.keys(confirmations).forEach((key) => {
-          if (confirmations[key] >= BTC_ACCEPTANCE) {
-            highConfirmations[key] = confirmations[key]
-          }
-        })
-        let newConfirmations = {}
-        for (let i = 0; i < transactionsWithLowConfirmations.length; i++) {
-          let res = await fetch(
-            `https://sochain.com/api/v2/is_tx_confirmed/BTC/${transactionsWithLowConfirmations[i].btcTxHash}`,
-          )
-            .then(handleErrors)
-            .then((response) => response.json())
-            .then((res: SoChainConfirmed) => res)
-            .catch((e) => {
-              showError('SoChain API error: ' + e.message)
-              RollbarErrorTracking.logErrorInRollbar(
-                'SoChain confirmations: ' + e.message,
-              )
-              return undefined
-            })
-
-          if (res === undefined) {
-            continue
-          }
-          newConfirmations[transactionsWithLowConfirmations[i].btcTxHash] =
-            res.data.confirmations
-        }
-        const confirmationsRecombined = {
-          ...highConfirmations,
-          ...newConfirmations,
-        }
-        setConfirmations(confirmationsRecombined)
-      }
-    }
-  }, POLL_DURATION_TXS)
 
   const handleLastRequestChange = (tx: Transaction) => {
     setLastRequest(tx)
     window.localStorage.setItem(account, JSON.stringify(tx))
+  }
+
+  const handleSetLastRequest = (tx: Transaction) => {
+    setLastRequest(tx)
   }
 
   const [onPresentBurn] = useModal(
@@ -329,11 +164,12 @@ const Home: React.FC = () => {
                 </Grid>
                 <Grid item xs={12} sm={12} md={8}>
                   <TransactionsTableContainer
-                    transactions={transactions}
-                    confirmations={confirmations}
-                    handleLoading={handleLoading}
-                    isLoading={isLoading}
+                    account={account}
+                    previousAccount={previousAccount}
                     lastRequest={lastRequest}
+                    handleSetLastRequest={handleSetLastRequest}
+                    checkAndRemoveLastRequest={checkAndRemoveLastRequest}
+                    wallet={wallet}
                   />
                 </Grid>
               </Grid>
