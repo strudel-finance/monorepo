6ab00b6658c65775cb7f3310587ca2184a0696ef
Clearwood <Clearwood@users.noreply.github.com>
2020-10-12 16:58:26 +0200

    added help menu entry (#65)

--- packages/frontend/src/components/TopBar/components/Nav.tsx
+++ packages/frontend/src/components/TopBar/components/Nav.tsx
@@ -17,11 +17,18 @@ const Nav: React.FC = () => {
         </StyledLink>
       )}
       <StyledAbsoluteLink
+        href="https://medium.com/@strudelfinance/how-to-bridge-the-bridge-679891dd0ae8"
+        target="_blank"
+      >
+        Help
+      </StyledAbsoluteLink>
+      <StyledAbsoluteLink
         href="https://medium.com/@strudelfinance/strudel-manifesto-580759f9634b"
         target="_blank"
       >
         About
       </StyledAbsoluteLink>
+
     </StyledNav>
   )
 }
