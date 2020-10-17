757f7ed65225581441148d74fc450d843bee9677
Clearwood <Clearwood@users.noreply.github.com>
2020-10-17 21:00:01 +0200

    changed apy calculation (#87)
    * changed apy calculation
    
    * flex fix
    
    * lint
    
    Co-authored-by: Keno Budde
    Co-authored-by: matic123matic <matic@proxima.si>
    Co-authored-by: MPmatic <slomatic@gmail.com>

--- packages/frontend/src/views/Farms/components/FarmCards.tsx
+++ packages/frontend/src/views/Farms/components/FarmCards.tsx
@@ -55,29 +55,16 @@ const FarmCards: React.FC = () => {
         ...farm,
         ...stakedValue[i],
         apy: stakedValue[i]
-          ? (() => {
-              if (i <= 2)
-                return strudelPrice
-                  .times(STRUDEL_PER_BLOCK)
-                  .times(BLOCKS_PER_YEAR)
-                  .times(stakedValue[i].poolWeight)
-                  .div(stakedValue[i].totalWethValue)
-
-              return strudelPrice
-                .times(STRUDEL_PER_BLOCK)
-                .times(BLOCKS_PER_YEAR)
-                .div(stakedValue[i].totalWethValue)
-            })()
+          ? strudelPrice
+              .times(STRUDEL_PER_BLOCK)
+              .times(BLOCKS_PER_YEAR)
+              .times(stakedValue[i].poolWeight)
+              .div(stakedValue[i].totalWethValue)
           : null,
         percentage: stakedValue[i]
-          ? (() => {
-              if (i <= 2)
-                return Number(Number(stakedValue[i].poolWeight) * Number(100))
-                  .toFixed(2)
-                  .toString()
-
-              return '0'
-            })()
+          ? Number(Number(stakedValue[i].poolWeight) * Number(100))
+              .toFixed(2)
+              .toString()
           : null,
       }
       const newFarmRows = [...farmRows]
@@ -99,7 +86,6 @@ const FarmCards: React.FC = () => {
             {farmRow.map((farm, j) => (
               <React.Fragment key={j}>
                 <FarmCard farm={farm} index={i + j} rowIndex={i} />
-                {(j <= 1 || (i === 1 && j !== 3)) && <StyledSpacer />}
               </React.Fragment>
             ))}
           </StyledRow>
@@ -294,8 +280,8 @@ const StyledCardAccent = styled.div`
 `
 
 const StyledCards = styled.div`
-  width: 1210px;
-  @media (max-width: 768px) {
+  width: 1240px;
+  @media (max-width: 1240px) {
     width: 100%;
   }
 `
@@ -321,7 +307,7 @@ const StyledRow = styled.div`
 `
 
 const StyledCardWrapper = styled.div`
-  display: flex;
+  margin: 12px;
   width: calc((900px - ${(props) => props.theme.spacing[4]}px * 2) / 3);
   position: relative;
 `
