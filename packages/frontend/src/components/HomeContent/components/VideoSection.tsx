6f67d76bd0f18965de12fd794f83d7d8b34b0e3b
devstar20 <ninjadev2020@gmail.com>
2021-02-11 17:44:52 +0200

    added video,

--- packages/frontend/src/components/HomeContent/components/VideoSection.tsx
+++ packages/frontend/src/components/HomeContent/components/VideoSection.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useCallback, useState, useMemo} from 'react'
 import styled from 'styled-components'
 import playImg from '../../../assets/img/mediaplayer.png'
 
-const VideoSection: React.FC = () => (
-  <>
+
+const VideoSection: React.FC = () => {
+  const [play, setPlay] = React.useState(false);
+  const url = play
+    ? `https://www.youtube.com/embed/LLEfTMxTvdI?autoplay=1`
+    : `https://www.youtube.com/embed/LLEfTMxTvdI`;
+ return(
+ <>
+ 
     <PlayerButton>
-        <img src={playImg} ></img>
+    <YoutubeVideoSection className={play? 'ShowSection': 'HiddenSection'}>
+    <iframe
+        width="800"
+        height="500"
+        src={url}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </YoutubeVideoSection>
+        <a onClick={() => setPlay(true)} className={play? 'YoutubeStarted': ''}><img src={playImg} ></img></a>
     </PlayerButton>
   </>
-)
-
+ );
+}
+const YoutubeVideoSection = styled.div`
+    width: 100%;
+    height: auto;
+`
 const PlayerButton = styled.div`
     display:flex;
     text-align:center;
