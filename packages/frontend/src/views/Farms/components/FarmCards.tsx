f36261b7440bd59f3628f55b5c617573a14528ec
Gazoh <cem@axastudios.com>
2021-03-02 23:47:44 +0100

    Changed the whole styling for the front-end

--- packages/frontend/src/views/Farms/components/FarmCards.tsx
+++ packages/frontend/src/views/Farms/components/FarmCards.tsx
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
 import React, { useEffect, useState } from 'react'
 import Countdown, { CountdownRenderProps } from 'react-countdown'
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
 import { useWallet } from 'use-wallet'
 import Button from '../../../components/Button'
 import Card from '../../../components/Card'
@@ -17,7 +17,9 @@ import useFarms from '../../../hooks/useFarms'
 import useVBTC from '../../../hooks/useVBTC'
 import { getEarned, getMasterChefContract } from '../../../vbtc/utils'
 import { bnToDec } from '../../../utils'
-import { StrudelMoving, VBTCSpin } from '../../../components/Lottie'
+import { VBTCSpin } from '../../../components/Lottie'
+import StrudleLogoIcon from '../../../assets/img/Strudel-logo-Icon.png'
+import BtcIcon from '../../../assets/img/Strudel-Bitcoin-Icon.png'
 
 interface FarmWithStakedValue extends Farm, StakedValue {
   isBalancer?: boolean
@@ -50,16 +52,15 @@ const FarmCards: React.FC = () => {
         ...stakedValue[i],
         apy: stakedValue[i]
           ? strudelPrice
-              .times(STRUDEL_PER_BLOCK)
-              .times(BLOCKS_PER_YEAR)
-              .times(stakedValue[i].poolWeight)
-              .times(stakedValue[i].multiplier)
-              .div(stakedValue[i].totalWethValue)
+            .times(STRUDEL_PER_BLOCK)
+            .times(BLOCKS_PER_YEAR)
+            .times(stakedValue[i].poolWeight)
+            .div(stakedValue[i].totalWethValue)
           : null,
         percentage: stakedValue[i]
           ? Number(Number(stakedValue[i].poolWeight) * Number(100))
-              .toFixed(2)
-              .toString()
+            .toFixed(2)
+            .toString()
           : null,
       }
       const newFarmRows = [...farmRows]
@@ -86,10 +87,10 @@ const FarmCards: React.FC = () => {
           </StyledRow>
         ))
       ) : (
-        <StyledLoadingWrapper>
-          <Loader text="Exploring new planets ..." />
-        </StyledLoadingWrapper>
-      )}
+          <StyledLoadingWrapper>
+            <Loader text="Exploring new planets ..." />
+          </StyledLoadingWrapper>
+        )}
     </StyledCards>
   )
 }
@@ -138,9 +139,9 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, index, rowIndex }) => {
   const getSymbol = (icon: string): any => {
     switch (icon) {
       case '1':
-        return <StrudelMoving />
+        return StrudleLogoIcon;
       case '2':
-        return <VBTCSpin />
+        return BtcIcon;
       case '3':
         return <VBTCSpin />
       default:
@@ -157,7 +158,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, index, rowIndex }) => {
         <CardContent>
           <StyledContent>
             <CardIcon>
-              <StyledMoving>{getSymbol(farm.icon)}</StyledMoving>
+              <img style={{ height: 48 }} src={getSymbol(farm.icon)} alt='icon'/>
             </CardIcon>
 
             <StyledTitle>{farm.name}</StyledTitle>
@@ -194,10 +195,10 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, index, rowIndex }) => {
               <span>
                 {farm.apy
                   ? `${farm.apy
-                      .times(new BigNumber(100))
-                      .toNumber()
-                      .toLocaleString('en-US')
-                      .slice(0, -1)}%`
+                    .times(new BigNumber(100))
+                    .toNumber()
+                    .toLocaleString('en-US')
+                    .slice(0, -1)}%`
                   : 'Loading ...'}
               </span>
               {/* <span>
@@ -220,11 +221,13 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, index, rowIndex }) => {
   )
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3dbef3c (Changed the whole styling for the front-end)
 const StyledCardAccent = styled.div`
   background-size: 300% 300%;
   border-radius: 12px;
-  filter: blur(6px);
   position: absolute;
   top: -2px;
   right: -2px;
